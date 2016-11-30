7eb6b823e33bf59e07485308fcb8108a1789833d
iarczi <artur.latoszewski@safekiddo.com>
2016-11-30 23:15:32 +0100

    Init

---
+++ app/src/main/java/pl/thecodeside/rxjavaweather/MainActivity.java
@@ -0,0 +1,13 @@
+package pl.thecodeside.rxjavaweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
